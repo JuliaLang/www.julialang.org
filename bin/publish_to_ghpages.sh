b945f5241ed2ad4588fcf83540c033230b9fca7f
Logan Kilpatrick <lok026@g.harvard.edu>
2020-01-11 21:29:25 -0500

    Update publish_to_ghpages.sh

--- bin/publish_to_ghpages.sh
+++ bin/publish_to_ghpages.sh
@@ -26,4 +26,4 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to github"
-git push gh-pages
+git push origin HEAD:gh-pages
