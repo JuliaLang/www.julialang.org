5f1099ffeccc142de18283c08248638a092ab4d1
Logan Kilpatrick <lok026@g.harvard.edu>
2020-01-11 21:34:01 -0500

    Update publish_to_ghpages.sh

--- bin/publish_to_ghpages.sh
+++ bin/publish_to_ghpages.sh
@@ -14,7 +14,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -26,4 +26,4 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to github"
-git push origin HEAD:gh-pages
+git push --all
