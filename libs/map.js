ef16ad230c2d0a9333ce2099f6e30382878ac284
ViralBShah <virals@gmail.com>
2026-03-06 05:03:46 +0000

    Deploying to gh-pages from @ JuliaLang/www.julialang.org@0d723d6b1651bf50b6470b491f06aa0cf9ce9d4e 🚀

--- libs/map.js
+++ libs/map.js
@@ -1,61 +1,54 @@
-'use strict'
+'use strict';
 
-function add_icon(service, account) {
+function addIcon(service, account) {
     if (service === 'homepage') {
         return `<a id="img-link" href="${account}" target="_blank" rel="noreferrer noopener"><i class="fa fa-globe" aria-hidden="true"></i></a> `;
-    } else {
-        return `<a id="img-link" href="${account}" target="_blank" rel="noreferrer noopener"><i class="fa fa-${service} fa-lg" aria-hidden="true"></i></a> `;
     }
+    return `<a id="img-link" href="${account}" target="_blank" rel="noreferrer noopener"><i class="fa fa-${service} fa-lg" aria-hidden="true"></i></a> `;
 }
 
+function popupText(group) {
+    let s = `<b>${group.name}</b></br>`;
 
-function popup_text(group) {
-    var s = `<b>${group.name}</b></br>`
-
-    if (group.homepage != undefined) {
-        s += add_icon('homepage', group.homepage);
+    if (group.homepage != null) {
+        s += addIcon('homepage', group.homepage);
     }
-
-    if (group.github != undefined) {
-        s += add_icon('github', group.github);
+    if (group.github != null) {
+        s += addIcon('github', group.github);
     }
-
-    if (group.gitlab != undefined) {
-        s += add_icon('gitlab', group.gitlab);
+    if (group.gitlab != null) {
+        s += addIcon('gitlab', group.gitlab);
     }
-
-    if (group.twitter != undefined) {
-        s += add_icon('twitter', group.twitter);
+    if (group.twitter != null) {
+        s += addIcon('twitter', group.twitter);
     }
-
-    if (group.linkedin != undefined) {
-        s += add_icon('linkedin', group.linkedin);
+    if (group.linkedin != null) {
+        s += addIcon('linkedin', group.linkedin);
     }
 
     return s;
 }
 
-function add_marker(markers, group) {
-    var marker = L.circleMarker([group.lat, group.lon], {
+function addMarker(markers, group) {
+    const marker = L.circleMarker([group.lat, group.lon], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
         radius: 10
     });
-    marker.bindPopup(popup_text(group));
-
+    marker.bindPopup(popupText(group));
     markers.addLayer(marker);
 }
 
 if (document.getElementById('map')) {
-    var map = L.map('map').setView([20, 15], 2);
+    const map = L.map('map').setView([20, 15], 2);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         noWrap: true
     }).addTo(map);
 
-    var markers = L.markerClusterGroup({
+    const markers = L.markerClusterGroup({
         iconCreateFunction: function(cluster) {
             return new L.DivIcon({
                 html: '<div><span>' + cluster.getChildCount() + '</span></div>',
@@ -67,7 +60,7 @@ if (document.getElementById('map')) {
     });
 
     for (const group of groups) {
-        add_marker(markers, group);
+        addMarker(markers, group);
     }
 
     map.addLayer(markers);
