5265da4d89e285cd0ac618c82a3966a6ceb9a10b
ViralBShah <virals@gmail.com>
2026-09-26 21:37:09 +0000

    Deploying to gh-pages from @ JuliaLang/www.julialang.org@be2b9a4e3eb55e041f2c9be7b76db777d0cd7f21 🚀

--- libs/groups.js
+++ libs/groups.js
@@ -72,4 +72,11 @@ const groups = [
         lon: -79.995888,
         homepage: 'https://events.cmu.edu/event/29322-julia-programming-user-meeting',
     },
+    {
+        name: 'Julia Yerevan',
+        lat: 40.193537, 
+        lon: 44.503473,
+        homepage: "https://juliacon.am",
+        linkedin: 'https://www.linkedin.com/company/julia-yerevan',
+    }
 ];
